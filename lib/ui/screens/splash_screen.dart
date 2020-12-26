135295eef98ad0d116173065618ec170bcf48227
MuhammadEssam96 <muhammad.essam96@gmail.com>
2020-12-26 21:30:23 +0200

    Add Splash Screen

--- lib/ui/screens/splash_screen.dart
+++ lib/ui/screens/splash_screen.dart
@@ -1,10 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:muslim_prayer_times/ui/values/colors.dart';
+import 'package:muslim_prayer_times/ui/values/styles.dart';
 
 class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(AppStyles.defaultSystemUiOverlayStyle);
     return Scaffold(
-
+      backgroundColor: AppColors.primaryColorLight,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16.0),
+              child: Image.asset(
+                "assets/images/kaaba_logo_brown.png",
+                width: MediaQuery.of(context).size.width * 0.50,
+              ),
+            ),
+            Text(
+              "Muslim Prayer Times",
+              style: TextStyle(
+                fontFamily: "Pacifico",
+                fontSize: 27,
+                color: AppColors.primaryColorDark
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/ui/values/styles.dart
+++ lib/ui/values/styles.dart
@@ -4,6 +4,8 @@ import 'package:muslim_prayer_times/ui/values/colors.dart';
 class AppStyles {
   static SystemUiOverlayStyle defaultSystemUiOverlayStyle = SystemUiOverlayStyle(
     statusBarColor: AppColors.primaryColorLight,
-    systemNavigationBarColor: AppColors.primaryColorLight
+    statusBarIconBrightness: Brightness.dark,
+    systemNavigationBarColor: AppColors.primaryColorLight,
+    systemNavigationBarIconBrightness: Brightness.light
   );
 }
